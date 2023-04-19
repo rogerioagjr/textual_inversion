80f3f2484595ba2649dfd874af45849ef4c5dc75
Rogério Júnior <rogerio.agjunior@gmail.com>
2023-04-19 16:05:49 -0700

    Update inference.py
    Changed number of inference steps from 50 to 10

--- inference.py
+++ inference.py
@@ -6,7 +6,7 @@ def main():
     pipe = StableDiffusionPipeline.from_pretrained(model_id,torch_dtype=torch.float16).to("cuda")
     prompt = "A <cat-toy> backpack"
 
-    image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+    image = pipe(prompt, num_inference_steps=10, guidance_scale=7.5).images[0]
 
     image.save("cat-backpack.png")
 
